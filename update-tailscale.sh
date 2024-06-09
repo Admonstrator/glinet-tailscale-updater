e32279271b474f9f7b8c81912a33ddbcdaeb7376
Aaron Viehl <hello@admon.in.ua>
2024-06-09 13:33:01 +0200

    Push version

--- update-tailscale.sh
+++ update-tailscale.sh
@@ -10,7 +10,7 @@
 # Author: Admon
 # Contributor: lwbt
 # Date: 2024-01-24
-SCRIPT_VERSION="2024.06.09.02"
+SCRIPT_VERSION="2024.06.09.03"
 SCRIPT_NAME="update-tailscale.sh"
 UPDATE_URL="https://raw.githubusercontent.com/Admonstrator/glinet-tailscale-updater/main/update-tailscale.sh"
 TAILSCALE_TINY_URL="https://github.com/Admonstrator/glinet-tailscale-updater/releases/latest/download/"
